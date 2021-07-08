85b81c4cbebbc176e9ec193dc8c1f4f0a0d91bb5
Jon Kivinen <226881+jkiv@users.noreply.github.com>
2021-07-07 20:12:06 -0400

    Refactoring and code clean-up.

--- src/b11y-jperdy/static/jperdy.js
+++ src/b11y-jperdy/static/jperdy.js
@@ -1,19 +1,20 @@
 
 var jperdy = jperdy || {};
 
-(function(context) {
+(function(context, settings) {
 
     context.timeouts = context.timeout || {};
-    context.state = context.state || {};
-    context.mqtt = context.mqtt || {};
+    context.state    = context.state   || {};
+    context.mqtt     = context.mqtt    || {};
 
-    context.CATEGORY_TIMEOUT_MS = 3000;
-    context.VALUE_TIMEOUT_MS = 2000;
-    context.ANSWER_TIMEOUT_MS = 60000;
-    context.QUESTION_TIMEOUT_MS = 5000;
-    context.WINNER_TIMEOUT_MS = 5000;
+    context.CATEGORY_TIMEOUT_MS =  3000;
+    context.VALUE_TIMEOUT_MS    =  2000;
+    context.ANSWER_TIMEOUT_MS   = 30000;
+    context.QUESTION_TIMEOUT_MS =  5000;
+    context.WINNER_TIMEOUT_MS   =  5000;
 
     context.RANDOM_QUESTION_URI = 'http://localhost:5000/answer';
+    context.PLAYER_POINTS_URI   = 'http://localhost:5000/score/{{channel}}/{{player}}'
 
     context.mqtt.initialize = function(hostname, port, server_path, client_id) {
         context.mqtt.client = new Paho.MQTT.Client(hostname, port, server_path, client_id);
@@ -21,12 +22,12 @@ var jperdy = jperdy || {};
     }
 
     context.mqtt.on_connect = function() {
-        console.log("[MQTT] Connected.");
+        console.log("[jperdy] MQTT connected.");
         context.mqtt.subscribe("b11y/j/*");
     }
 
     context.mqtt.on_disconnect = function(responseObject) {
-        console.log("[MQTT] Disconnected... (" + responseObject.errorMessage + ")");
+        console.log("[jperdy] MQTT disconnected... (" + responseObject.errorMessage + ")");
 
         // Reconnect
         context.mqtt.connect(
@@ -37,7 +38,7 @@ var jperdy = jperdy || {};
     }
 
     context.mqtt.connect = function(username, password, useSSL=false) {
-        console.log("[MQTT] Connecting as '" + username + "'...");
+        console.log("[jperdy] MQTT connecting as '" + username + "'...");
 
         context.mqtt.auth = context.mqtt.auth || {};
         context.mqtt.auth.username = username;
@@ -51,15 +52,20 @@ var jperdy = jperdy || {};
             onSuccess: context.mqtt.on_connect
         });
     }
-    
+
     context.mqtt.subscribe = function(topic) {
-        console.log("[MQTT] Subscribing to '" + topic + "'");
+        console.log("[jperdy] MQTT - Subscribing to '" + topic + "'");
         context.mqtt.client.subscribe(topic);
     }
 
-    context.mqtt.on_message = function(message) {
-        console.log('[MQTT] Message received: topic=' + message.destinationName + ', payload=' + message.payloadString);
+    context.mqtt.send = function(topic, message) {
+        // TODO
+    }
 
+    context.mqtt.on_message = function(message) {
+        console.log('[jperdy] MQTT message received: topic=' + message.destinationName + ', payload=' + message.payloadString);
+
+        // Handle messages in IDLE state
         handle_idle = function(message) {
             topic = message.destinationName;
             payload = message.payloadString;
@@ -78,7 +84,8 @@ var jperdy = jperdy || {};
             }
         }
 
-        handle_guesses = function(message) {
+        // Handle messages in ACCEPTING_QUESTIONS state
+        handle_accepting_questions = function(message) {
             topic = message.destinationName;
             payload = message.payloadString;
 
@@ -98,22 +105,66 @@ var jperdy = jperdy || {};
         }
 
         // Handle incoming messaged based on current game state
+
+        console.log('[jperdy] Current state: ' + context.state);
+
         switch(context.state)
         {
             case context.states.IDLE:
                 handle_idle(message);
                 break;
             case context.states.ACCEPTING_QUESTIONS:
-                handle_guesses(message);
+                handle_accepting_questions(message);
+                break;
             default:
                 // (do nothing)
         }
 
-        console.log('[jperdy] Current state: ' + context.state);
+    }
+    
+    context._set_game_text = function(text_unsafe) {
+        // Escape unsafe text  (TODO use more robust method?)
+        text_safeish = text_unsafe.replace(/&/g, "&amp;")
+                                  .replace(/</g, "&lt;")
+                                  .replace(/>/g, "&gt;")
+                                  .replace(/"/g, "&quot;")
+                                  .replace(/'/g, "&#039;");
 
+        container = document.getElementById('jperdy-text');
+        container.innerHTML = text_safeish;
+    }
+
+    context._show_background = function(show) {
+        if (show) {
+            document.body.classList.add('jperdy-active');
+        }
+        else {
+            document.body.classList.remove('jperdy-active');
+        }
+    }
+
+    context.reset_game_state = function() {
+        // Clear the screen
+        console.log("[jperdy] Resetting game state to IDLE...");
+        context._set_game_text('');
+        context._show_background(false);
+
+        // Clear round context
+        context.round_info = {
+            answer: null,
+            initiator: null,
+            channel: null
+        };
+
+        context.state = context.states.IDLE;
     }
 
     context.start_round = function(channel, username) {
+        
+        // TODO if not IDLE state, return
+
+        console.log("[jperdy] Round initiated by '" + username + "' in '" + channel + "'.");
+
         // Save the round state
         context.round_info = {
             channel: channel,
@@ -130,26 +181,28 @@ var jperdy = jperdy || {};
 
                 // Check response status
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    console.log('[jperdy] Could not retreive random question from ' + context.RANDOM_QUESTION_URI, reponse);
+                    throw new Error('Could not retreive random question from ' + context.RANDOM_QUESTION_URI);
                 }
 
                 return response.text();
             })
-            .then(response_body => {
-                console.log(response_body);
-                
+            .then(response_body => {                
                 // Unpack answer
                 answer = JSON.parse(response_body);
                 context.round_info.answer = answer;
 
+                console.log('[jperdy] New answer: ', answer);
+
+                // Enable background colour
+                context._show_background(true);
+
                 // Show category
-                console.log("Showing category: " + answer.category.toUpperCase());
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = answer.category.toUpperCase();
-                // TODO set background-color: jeopardy-blue
+                console.log("[jperdy] Displaying category, '" + answer.category + "'");
+                context._set_game_text(answer.category);
 
                 // Send MQTT response
-                // TODO
+                context.mqtt.send('b11y/j/round_start', ''); // TODO
 
                 // Start timeout
                 return new Promise((resolve, reject) => {
@@ -159,11 +212,9 @@ var jperdy = jperdy || {};
                 });
             })
             .then(() => {
-
                 // Show value
-                console.log("Showing value: " + context.round_info.answer.value);
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = '$' + context.round_info.answer.value;
+                console.log("[jperdy] Displaying value, $" + context.round_info.answer.value);
+                context._set_game_text('$' + context.round_info.answer.value);
 
                 // Start timeout
                 return new Promise((resolve, reject) => {
@@ -174,38 +225,39 @@ var jperdy = jperdy || {};
             })
             .then(() => {
                 // Show answer
-                console.log("Showing answer: " + context.round_info.answer.answer);
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = context.round_info.answer.answer;
+                console.log("[jperdy] Displaying answer, '" + context.round_info.answer.answer + "'");
+                context._set_game_text(context.round_info.answer.answer);
 
                 // Update state
                 context.state = context.states.ACCEPTING_QUESTIONS;
 
                 // Start timeout
-                context.timeouts.answer = setTimeout(context.on_question_timeout, context.ANSWER_TIMEOUT_MS);
+                context.timeouts.answer = setTimeout(context.on_round_timeout, context.ANSWER_TIMEOUT_MS);
             })
             .catch(error => {
-                context.state = context.states.IDLE;
-                console.error('There has been a problem with your fetch operation:', error);
+                console.error("[jperdy] Error while handling round start.", error);
+                context.reset_game_state();
             });
     }
 
-    context.on_question_timeout = function() {
-        console.log("Answer timeout! (state=" + context.state + ")");
+    context.on_round_timeout = function() {
+        console.log("[jperdy] Round time-out!");
 
         if (context.state != context.states.ACCEPTING_QUESTIONS) {
-            console.log("Not in correct state.");
+            console.error("[jperdy] Round time-out occured but in wrong state. state = " + context.state);
             return;
         }
 
         // Update state
         context.state = context.states.QUESTION_TIMEOUT;
 
+        // Kick of MQTT message
+        context.mqtt.send('b11y/j/round_end', ''); // TODO
+
         (function() {
             // Show message
-            console.log("Showing answer");
-            container = document.getElementById('jperdy-text');
-            container.innerHTML = context.round_info.answer.question;
+            console.log("[jperdy] Showing question, '" + context.round_info.answer.question + "'");
+            context._set_game_text(context.round_info.answer.question);
 
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -213,37 +265,29 @@ var jperdy = jperdy || {};
                 }, context.QUESTION_TIMEOUT_MS);
             });
         })()
-            .then(() => {
-                // Clear the screen
-                console.log("Clearing screen.");
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = '';
-                // TODO background-color = none
-
-                // Clear round context
-                context.round_info = {
-                    answer: null,
-                    initiator: null,
-                    channel: null
-                };
-                context.state = context.states.IDLE;
-            });
+        .then(context.reset_game_state)
+        .catch(error => {
+            console.error("[jperdy] Error while handling round time-out.", error);
+            context.reset_game_state();
+        });
     }
+
 
     context.on_guess = function(channel, username, guess) {
 
+        // TODO Fair, fuzzy matching
         is_guess_close_enough = function(guess, question) {
-            // TODO properly
             guess = guess.replace(/[^a-zA-Z0-9]/g, '');
             question = question.replace(/[^a-zA-Z0-9]/g, '');
 
-            console.log("guess=" + guess + ", question=" + question);
+            console.log("[jperdy] Checking guess = '" + guess + "' against question = '" + question + "'");
 
             return (guess == question);
         }
 
         // Test `guess` against `question_cleaned`
         if (!is_guess_close_enough(guess, context.round_info.answer.question_cleaned)) {
+            console.log("[jperdy] Guess '" + guess + "' was deemed incorrect.");
             return; // Incorrect guess
         }
 
@@ -251,7 +295,7 @@ var jperdy = jperdy || {};
         context.state = context.states.CORRECT_GUESS;
 
         // Kick of MQTT message
-        // TODO
+        context.mqtt.send('b11y/j/round_end', ''); // TODO
 
         // Stop answer/question timeout
         clearTimeout(context.timeouts.answer);
@@ -259,9 +303,8 @@ var jperdy = jperdy || {};
         // Update screen with user's name
         (function() {
             // Show answer
-            console.log("Showing answer");
-            container = document.getElementById('jperdy-text');
-            container.innerHTML = context.round_info.answer.question;
+            console.log("[jperdy] Showing question, '" + context.round_info.answer.question + "'");
+            context._set_game_text(context.round_info.answer.question);
 
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -271,9 +314,8 @@ var jperdy = jperdy || {};
         })()
             .then(() => {
                 // Show winner
-                console.log("Showing winner");
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = username;
+                console.log("[jperdy] Showing winner, '" + username + "'");
+                context._set_game_text(username);
 
                 return new Promise((resolve, reject) => {
                     setTimeout(() => {
@@ -281,23 +323,10 @@ var jperdy = jperdy || {};
                     }, context.WINNER_TIMEOUT_MS);
                 });
             })
-            .then(() => {
-                // Clear the screen
-                console.log("Clearing screen.");
-                container = document.getElementById('jperdy-text');
-                container.innerHTML = '';
-                // TODO background-color = none
-
-                console.log("Player \'"+username+"\' +$"+context.round_info.answer.value.toString());
-                // TODO return fetch(context.UPDATE_PLAYER_POINTS_URI)
-                
-                // Clear round context
-                context.state = context.states.IDLE;
-                context.round_info = {
-                    answer: null,
-                    initiator: null,
-                    channel: null
-                };
+            .then(context.reset_game_state)
+            .catch(error => {
+                console.error("[jperdy] Error while handling round end.", error);
+                context.reset_game_state();
             });
     }
 
@@ -307,17 +336,9 @@ var jperdy = jperdy || {};
         FETCHING_ANSWER: 'FETCHING_ANSWER',
         ACCEPTING_QUESTIONS: 'ACCEPTING_QUESTIONS',
         QUESTION_TIMEOUT: 'QUESTION_TIMEOUT',
-        CORRECT_GUESS: 'CORRECT_GUESS',
-        // ...
+        CORRECT_GUESS: 'CORRECT_GUESS'
     };
 
     context.state = context.states.IDLE;
 
 })(jperdy);
-
-
-// > Start message
-// < Question info
-// > Wager (daily double)
-// > Guesses
-// < Round done (correct guess or timeout)
